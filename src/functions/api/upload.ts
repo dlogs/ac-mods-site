a1a01b52a354796aec5e1a1e057ca45e6793f13d
David Logsdon <dlogs45@gmail.com>
2024-02-15 21:04:59 -0500

    fix insert sql

--- src/functions/api/upload.ts
+++ src/functions/api/upload.ts
@@ -23,7 +23,7 @@ INSERT INTO uploads (
   file_name,
   uploaded_at,
   uploaded_by_id
-) values (?, ?, ?, ?, ?, ?, ?)
+) values (?, ?, ?, ?, ?, ?, ?, ?)
 `
 
 export const onRequestPost: PagesFunction<Env> = async ({ request, env }): Promise<Response> => {
@@ -38,23 +38,11 @@ export const onRequestPost: PagesFunction<Env> = async ({ request, env }): Promi
 }
 
 const insertUserIfNeeded = async (db: D1Database, user: TokenUser): Promise<void> => {
-  const existsQueryRecord = await db.prepare(userExistsSql).bind(user.sub).first()
-  const count = existsQueryRecord?.count
-  if (count === 0) {
-    await db.prepare(insertUserSql).bind(user.sub, user.email, Date.now()).run()
-  } else if (count === null) {
-    throw new Error('Error checking if user exists')
-  }
+  await db.prepare(insertUserSql).bind(user.sub, user.email, Date.now()).run()
 }
 
-const userExistsSql = `
-SELECT count(*) as count
-FROM users
-WHERE id = ?
-`
-
 const insertUserSql = `
-INSERT INTO users (
+INSERT OR IGNORE INTO users (
   id,
   email,
   created_at
